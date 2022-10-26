30005ccc0c2565a88f97109b90d012ac6a3f60c0
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-10-26 17:47:04 +0200

    added store user validation

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_name' => 'required|unique:users|min:3',
+            'email' => 'required|email|unique:users',
+            'number' => 'required|numeric|digits:10',
+            'birthday' => 'required',
+            'password' => 'required|min:5',
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+
+            'success'   => false,
+
+            'message'   => 'Validation errors',
+
+            'data'      => $validator->errors()
+
+        ]));
+    }
+}
