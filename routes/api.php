58a38580a627446b75eba2c49487fd098368cca6
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-10-26 17:39:19 +0200

    Add user api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/users", [UserController::class,'index']);
+Route::post("/register", [UserController::class,'register']);
+Route::delete("/user/{userid}", [UserController::class,'destroy']);
+Route::get("/user/{userid}", [UserController::class,'show']);
+Route::patch("/user/{userid}", [UserController::class,'update']);
+Route::post("/login", [UserController::class,'login']);
